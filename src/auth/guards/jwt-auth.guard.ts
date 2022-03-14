2d7dd01681e7dde9451585835fc5435165dd0407
commin-pg <khm.commin@gmail.com>
2022-03-14 17:39:53 +0900

    .

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -12,47 +12,52 @@ const jwtConfig = config.get("jwt");
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
-//   constructor(private jwtService: JwtService) {
-//     super();
-//   }
-//   canActivate(context: ExecutionContext) {
-//     const request = context.switchToHttp().getRequest();
 
-//     const { authorization } = request.headers;
+    
+  constructor(private jwtService: JwtService) {
+    super();
+  }
 
-//     if (authorization === undefined) {
-//       throw new HttpException("Token 전송 안됨", HttpStatus.UNAUTHORIZED);
-//     }
 
-//     const token = authorization.replace("Bearer ", "");
-//     request.user = this.validateToken(token);
-//     return true;
-//   }
 
-//   validateToken(token: string) {
-//     // const secretKey = process.env.JWT_SECRET_KEY ? process.env.JWT_SECRET_KEY : 'dev';
-//     const secretKey = jwtConfig.secret;
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
 
-//     try {
-//       console.log(this.jwtService);
-//       const verify = this.jwtService.verify(token, { secret: secretKey });
-//       return verify;
-//     } catch (e) {
-//       console.log(e);
-//       switch (e.message) {
-//         // 토큰에 대한 오류를 판단합니다.
-//         case "INVALID_TOKEN":
-//         case "TOKEN_IS_ARRAY":
-//         case "NO_USER":
-//           throw new HttpException("유효하지 않은 토큰입니다.", 401);
+    const { authorization } = request.headers;
 
-//         case "EXPIRED_TOKEN":
-//         case "jwt expired":
-//           throw new HttpException("토큰이 만료되었습니다.", 410);
+    if (authorization === undefined) {
+      throw new HttpException("Token 전송 안됨", HttpStatus.UNAUTHORIZED);
+    }
 
-//         default:
-//           throw new HttpException("서버 오류입니다.", 500);
-//       }
-//     }
-//   }
+    const token = authorization.replace("Bearer ", "");
+    request.user = this.validateToken(token);
+    return true;
+  }
+
+  validateToken(token: string) {
+    // const secretKey = process.env.JWT_SECRET_KEY ? process.env.JWT_SECRET_KEY : 'dev';
+    const secretKey = jwtConfig.secret;
+
+    try {
+      console.log(this.jwtService);
+      const verify = this.jwtService.verify(token, { secret: secretKey });
+      return verify;
+    } catch (e) {
+      console.log(e);
+      switch (e.message) {
+        // 토큰에 대한 오류를 판단합니다.
+        case "INVALID_TOKEN":
+        case "TOKEN_IS_ARRAY":
+        case "NO_USER":
+          throw new HttpException("유효하지 않은 토큰입니다.", 401);
+
+        case "EXPIRED_TOKEN":
+        case "jwt expired":
+          throw new HttpException("토큰이 만료되었습니다.", 410);
+
+        default:
+          throw new HttpException("서버 오류입니다.", 500);
+      }
+    }
+  }
 }
