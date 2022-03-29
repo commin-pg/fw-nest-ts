f42de868cd3fa64173facbd87324ac56f2f66121
commin-pg <khm.commin@gmail.com>
2022-03-29 19:45:30 +0900

    ...

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NestCrawlerModule } from 'nest-crawler';
+
 import { AuthModule } from 'src/auth/auth.module';
 import { Finance } from './entity/finance.entity';
 import { FinanceController } from './finance.controller';
@@ -8,7 +8,7 @@ import { FinanceService } from './finance.service';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Finance]),
-  AuthModule,NestCrawlerModule],
+  AuthModule],
   controllers: [FinanceController],
   providers: [FinanceService]
 })
--- src/finance/finance.service.ts
+++ src/finance/finance.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Request } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { NestCrawlerService } from 'nest-crawler';
+
 import { Repository } from 'typeorm';
 import { Finance } from './entity/finance.entity';
 import * as cheerio from 'cheerio';
+import * as sanitizeHtml from 'sanitize-html';
 
 export enum FinanceType {
   KOSPI = '0',
@@ -17,43 +18,68 @@ export const FINANCE_BASE_TABLE_URL: string =
 export class FinanceService {
   constructor(
     @InjectRepository(Finance) private financeRepository: Repository<Finance>,
-    private readonly crwalerService: NestCrawlerService,
+
   ) {}
 
   async crwaling() {
-    for (var i = 0; i < 2; i++) {
+    for (var i = 0; i < 1; i++) {
       for (var idx = 1; idx < 5; idx++) {
         const BASE_URL =
           FINANCE_BASE_TABLE_URL +
           '?sosok=' +
-          (i === 0 ? FinanceType.KOSDAQ : FinanceType.KOSPI) +
+          (i === 0 ? FinanceType.KOSPI : FinanceType.KOSDAQ) +
           '&page=' +
           idx;
         // "table.type_2 > tbody > tr"
-        interface HTML{
-            html:string
-        }
-        const html: HTML = await this.crwalerService.fetch({
-          target: BASE_URL,
-          fetch: {
-            html : {
-                selector:'html',
-                how:'html'
-            }
-          },
-        });
-        console.log(html);
 
-        
+        // interface HTML {
+        //   html: string;
+        // }
 
-        cheerio.load(html.html)('table.type_2 > tbody > tr')
-        .map((index, ele)=>{
-            // console.log(index, ele.text);
-        })
+        // const html: HTML = await this.crwalerService.fetch({
+        //   target: BASE_URL,
+
+        //   fetch: {
+        //     html: {
+        //       selector: 'html',
+        //       how: 'html',
+        //     },
+        //   },
+        // });
+        // console.log(html);
+
+        // var cnt = 0;
+        // cheerio
+        //   .load(html.html, { decodeEntities: true })(
+        //     'table.type_2 > tbody > tr',
+        //   )
+        //   .map((index, ele) => {
+
+        // console.log(index, cheerio.load(ele)('td > a.title').text());
+        // console.log(index, cheerio.load(ele)('td > a.title').text());
+        // if (cheerio.load(ele)('td > a').attr('href')) {
+        //   cnt = cnt + 1;
+        //   console.log(cnt, cheerio.load(ele)('td.no').text());
+        //   console.log(cnt, cheerio.load(ele)('td > a').attr('href'));
+        //   console.log(ele)
+        //   console.log(
+        //     cnt,
+        //     sanitizeHtml(
+        //       cheerio.load(ele, { decodeEntities: true })('td > a').html(),
+        //       { parser: { decodeEntities: true } },
+        //     ),
+        //   );
+
+        //   console.log(
+        //     cnt,
+        //     sanitizeHtml(
+        //       '<html>가나다</html>',
+        //       { parser: { decodeEntities: true } },
+        //     ),
+        //   );
+        // }
+        // });
         // cheerio.load
-
-
-        
       }
     }
   }
